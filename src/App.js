61336948fdad9abf8e6866ef8a8eebd2ba06f42b
followname <merkonjb@gmail.com>
2021-04-03 21:59:22 +0300

    simple_table

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
+import React from 'react';
 import './App.css';
+import { Table } from './components/Table'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-         Hello
-        </p>
-      </header>
+    <div className='App'>
+      <Table />
     </div>
   );
 }
---
+++ src/components/columns.js
@@ -0,0 +1,42 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'Имя',
+        accessor: 'first_name'
+    },
+    {
+        Header: 'Фамилия',
+        accessor: 'last_name'
+    },
+    {
+        Header: 'Отчество',
+        accessor: 'third_name'
+    },
+    {
+        Header: 'Дата рождения',
+        accessor: 'birth_date'
+    },
+    {
+        Header: 'Математика',
+        accessor: 'math'
+    },
+    {
+        Header: 'Философия',
+        accessor: 'phylosophy'
+    },
+    {
+        Header: 'Русский',
+        accessor: 'russian'
+    },
+    {
+        Header: 'Английский',
+        accessor: 'english'
+    },
+    {
+        Header: 'Китайский',
+        accessor: 'chinese'
+    }
+]
